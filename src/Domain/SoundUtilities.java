3e4e803aadddc19965d50c79145a4c4260cd0e23
JacobBonefeld <jacobbonefeld@live.dk>
2020-03-01 22:34:53 +0100

    Code Refactoring

---
+++ src/Domain/SoundUtilities.java
@@ -0,0 +1,62 @@
+package Domain;
+
+import java.util.Random;
+
+public abstract class SoundUtilities {
+
+    private static final AudioPlayer FOOD_SLURP = new AudioPlayer("src/Resources/Sound/Slurping+1.wav");
+    private static final AudioPlayer FOOD_BITE = new AudioPlayer("src/Resources/Sound/bite.wav");
+    private static final AudioPlayer FOOD_CHOMP = new AudioPlayer("src/Resources/Sound/Chomp+1.wav");
+    private static final AudioPlayer MENU_SOUND = new AudioPlayer("src/Resources/Sound/MenuSound.wav", 0.1);
+    private static final AudioPlayer GAME_SOUND = new AudioPlayer("src/Resources/Sound/GameSound.wav", 0.1);
+    private static final AudioPlayer GAME_OVER = new AudioPlayer("src/Resources/Sound/GameOver2.wav");
+    private static final AudioPlayer VICTORY = new AudioPlayer("src/Resources/Sound/victoryFanfareCut1.wav");
+
+    public static void playGameSound(Boolean play) {
+        if (play) {
+            GAME_SOUND.play(-1);
+        } else {
+            GAME_SOUND.stop();
+        }
+
+    }
+
+    public static void playMenuSound(Boolean play) {
+        if (play) {
+            MENU_SOUND.play(-1);
+        } else {
+            MENU_SOUND.stop();
+        }
+    }
+
+    public static void playGameOverSound(Boolean play) {
+        if (play) {
+            GAME_OVER.play(0);
+        } else {
+            GAME_OVER.stop();
+        }
+    }
+
+    public static void playVictorySound(Boolean play) {
+        if (play) {
+            VICTORY.play(3);
+        } else {
+            VICTORY.stop();
+        }
+    }
+
+    public static void playRandomFoodSound() {
+        Random r = new Random();
+        switch (r.nextInt(3)) {
+            case 0:
+                FOOD_BITE.play(0);
+                break;
+            case 1:
+                FOOD_CHOMP.play(0);
+                break;
+            case 2:
+                FOOD_SLURP.play(0);
+                break;
+        }
+    }
+}
